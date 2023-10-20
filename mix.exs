066bb5cad62589d4eb44f34a713f83d8389f3914
BrooklinJazz <brooklinjazzmyers@gmail.com>
2023-10-20 11:49:10 -0400

    add description to package config

--- mix.exs
+++ mix.exs
@@ -1,5 +1,6 @@
 defmodule KinoLiveView.MixProject do
   use Mix.Project
+  @source_url "https://github.com/brooklinjazz/kino_live_view"
 
   def project do
     [
@@ -7,7 +8,8 @@ defmodule KinoLiveView.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -25,5 +27,17 @@ defmodule KinoLiveView.MixProject do
       {:phoenix, "~> 1.7.7"},
       {:ex_doc, "~> 0.30.9"},
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Brooklin Myers"],
+      description: "Dynamically Inject LiveViews into a Phoenix application from within Livebook",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      },
+      source_url: @source_url
+    }
   end
 end
